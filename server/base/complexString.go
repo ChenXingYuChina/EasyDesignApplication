484464f02b484df055a1239b3491cb4bd625d09c
ChenXingYuChina <chen_xingyu2019@126.com>
2019-12-11 16:23:51 +0800

    finish base level

--- server/base/complexString.go
+++ server/base/complexString.go
@@ -1,7 +1,10 @@
 package base
 
 import (
+	"encoding/binary"
 	"encoding/json"
+	"io"
+	"os"
 )
 
 type resources struct {
@@ -22,4 +25,83 @@ type ComplexString struct {
 	Widths      []int32   `json:"width"`
 	ResourcesId resources `json:"resources"`
 }
+
+func (s *ComplexString) SaveComplexStringToFile(fileName string) (err error) {
+	f, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	err = s.SaveComplexString(f)
+	if err != nil {
+		return err
+	}
+	return f.Close()
+}
+
+func (s *ComplexString) SaveComplexString(w io.Writer) (err error) {
+	help := int64(len(s.Content)) + (int64(len(s.Positions)) << 32)
+	err = binary.Write(w, binary.LittleEndian, help)
+	if err != nil {
+		return err
+	}
+	_, err = w.Write([]byte(s.Content))
+	if err != nil {
+		return err
+	}
+	err = binary.Write(w, binary.LittleEndian, s.Positions)
+	if err != nil {
+		return err
+	}
+	err = binary.Write(w, binary.LittleEndian, s.Widths)
+	if err != nil {
+		return err
+	}
+	return binary.Write(w, binary.LittleEndian, s.ResourcesId.r)
+}
+
+func LoadComplexStringFromFile(fileName string) (goal *ComplexString, err error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return
+	}
+	goal, err = LoadComplexString(f)
+	if err != nil {
+		return nil, err
+	}
+	return goal, f.Close()
+}
+
+func LoadComplexString(r io.Reader) (goal *ComplexString, err error) {
+	var help int64
+	err = binary.Read(r, binary.LittleEndian, &help)
+	if err != nil {
+		return nil, err
+	}
+	lLow := int(int32(help))
+	lHigh := int(int32(help >> 32))
+	goal = &ComplexString{
+		Content:string(make([]byte, lLow)),
+		Positions:make([]int32, lHigh),
+		Widths:make([]int32, lHigh),
+		ResourcesId:resources{r:make([]int64, lHigh)},
+	}
+	_, err = r.Read([]byte(goal.Content))
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Read(r, binary.LittleEndian, goal.Positions)
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Read(r, binary.LittleEndian, goal.Widths)
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Read(r, binary.LittleEndian, goal.ResourcesId.r)
+	if err != nil {
+		return nil, err
+	}
+	return goal, nil
+}
+
 
