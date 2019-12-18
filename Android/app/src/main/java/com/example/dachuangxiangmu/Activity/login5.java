416690cd0d44446034ddfa64d326c94ca12b0d3f
Bowen-Sun <bosun996@gmail.com>
2019-12-18 12:22:07 +0800

    前端

--- Android/app/src/main/java/com/example/dachuangxiangmu/Activity/login5.java
+++ Android/app/src/main/java/com/example/dachuangxiangmu/Activity/login5.java
@@ -2,7 +2,6 @@ package com.example.dachuangxiangmu.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.TextureView;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -11,14 +10,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.dachuangxiangmu.R;
 
 public class login5 extends AppCompatActivity {
-    private TextureView textureView;
+//    private TextureView textureView;
     private ImageView next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login5);
         next = findViewById(R.id.next);
-        textureView = (TextureView) findViewById(R.id.textureView);
+//        textureView = (TextureView) findViewById(R.id.textureView);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
