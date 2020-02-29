741d0c59b79658c79ff0e73e922ef779e913afb3
ChenXingYuChina <chen_xingyu2019@126.com>
2020-02-29 21:19:03 +0800

    add remote run version

--- server/base/user/prepare.go
+++ server/base/user/prepare.go
@@ -8,7 +8,11 @@ import (
 func init() {
 	base.RegisterPrepare(func() {
 		PrepareUserDir()
-		p, err := prepareUserSQL()
+		p, err := userSQLPrepare()
+		if err != nil {
+			panic(fmt.Sprint("user base sql fail: ", err, p))
+		}
+		p, err = prepareUserSQL()
 		if err != nil {
 			panic(fmt.Sprint("user full sql fail: ", err, p))
 		}
@@ -20,10 +24,6 @@ func init() {
 		p, err = userMiniSQLPrepare()
 		if err != nil {
 			panic(fmt.Sprint("user mini sql fail: ", err, p))
-		}
-		p, err = userSQLPrepare()
-		if err != nil {
-			panic(fmt.Sprint("user base sql fail: ", err, p))
 		}
 		p, err = prepareUserAction()
 		if err != nil {
